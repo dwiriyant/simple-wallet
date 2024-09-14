6ea05ad82a2d02fd9eb0b67be413344562f7f08d
dwiriyan <dwiriyan855@gmail.com>
2024-09-14 13:41:31 +0700

    feat: install godotenv & initialize db connection

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"net/http"
+	"simple-wallet/db"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	db.Init()
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Simple Wallet REST API")
