be94ec890fead06bf3438b6004688634626c42b5
dwiriyan <dwiriyan855@gmail.com>
2024-09-17 10:50:20 +0700

    feat: Add graceful shutdown

--- main.go
+++ main.go
@@ -1,18 +1,43 @@
 package main
 
 import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
 	"simple-wallet/db"
+	"simple-wallet/migration"
 	"simple-wallet/routes"
+	"time"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
 )
 
 func main() {
 	db.Init()
+	migration.Migrate()
 
 	e := echo.New()
 
 	routes.API(e)
 
-	e.Logger.Fatal(e.Start(":8081"))
+	e.Logger.SetLevel(log.INFO)
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+	// Start server
+	go func() {
+		if err := e.Start(":8081"); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds.
+	<-ctx.Done()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
